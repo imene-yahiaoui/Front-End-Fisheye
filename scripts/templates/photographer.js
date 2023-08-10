875a896b7c50fe6c0ac179b20af2afbd8a70c720
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-08-10 19:57:17 +0200

    ajout les photos et Intégrer la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,31 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait , id, city,country,tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+     
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+const span= document.createElement( 'span' );
+span.textContent = `${city}, `;
+const span1= document.createElement( 'span' );
+span1.textContent = country;
+const  P1= document.createElement( 'p' );
+P1.textContent = tagline;
+const  P2= document.createElement( 'p' );
+P2.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(span);
+        article.appendChild(span1);
+        article.appendChild(P1);
+        article.appendChild(P2);
         return (article);
     }
     return { name, picture, getUserCardDOM }
