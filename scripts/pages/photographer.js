e528631dc3f4e26b9bbcea4b3f1cd2095257dfeb
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-08-11 18:35:59 +0200

    ajout les photos de media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -39,32 +39,85 @@ const processData = async () => {
   if (profileData) {
     const photographer = profileData.photographers[0];
     profile(photographer);
+    profileData.media.forEach(ProfileMedia); // Appeler ProfileMedia pour chaque média
   }
 };
 
 processData();
 
+//function pour créer les éléments HTML
 function profile(data) {
-  const { name, portrait, id, city, country, tagline, price } = data;
+  const { name, portrait, city, country, tagline } = data;
   const picture = `assets/photographers/${portrait}`;
-  //cree les information de photographers
+
   const photographerInfo = `
     <div id="cader">
       <h2>${name}</h2>
       <p>${city}, ${country}</p>
       <p>${tagline}</p>
-        </div>
+    </div>
   `;
 
   document
     .getElementById("main_contact")
     .insertAdjacentHTML("afterbegin", photographerInfo);
-  //cree l'image de photographers
-const photographerImg = `
-<img class="photographerImg"  src=${picture} alt=${name} >
-`;
-document
-.getElementById("main_contact")
-.insertAdjacentHTML("beforeend", photographerImg );
+
+  const photographerImg = `
+    <img class="photographerImg" src=${picture} alt=${name}>
+  `;
+  document
+    .getElementById("main_contact")
+    .insertAdjacentHTML("beforeend", photographerImg);
+}
+
+//cree les médias
+function ProfileMedia(media) {
+  const { id, image, title,photographerId,video } = media;
+
+  const photo = `../../assets/images/photos/${photographerId}/${image}
+  `;
+  const videoMedia = `../../assets/images/photos/${photographerId}/${video}`;
+
+  if (video === undefined) {
+  const mediaPhotographe = `
+  <div class="media">
+  <figure class="media-info">
+    <img class="photographerImg" src=${photo} alt=${title}>
+    <figcaption class="media-title">${title}</figcaption>
+    <div class="like">
+<p> ${likes} </p>
+<i class="fa-solid fa-heart"></i>
+    </div>
+    </figure>
+    </div>
+  `;
+  document
+  .getElementById("main")
+  .insertAdjacentHTML("beforeend", mediaPhotographe);
+  } 
+
+ 
+else{
+ 
+  const mediaPhotographe = `
+  <div class="media">
+  <figure class="media-info">
+    <video  width="320" height="240" autoplay class="video" src=${videoMedia} type="video/mp4" >
+
+    <figcaption class="media-title">${title}</figcaption>
+    <div class="like">
+<p> ${likes} </p>
+<i class="fa-solid fa-heart"></i>
+    </div>
+    </figure>
+    </div>
+  `;
+
+
+
+  document
+  .getElementById("main")
+  .insertAdjacentHTML("beforeend", mediaPhotographe);
+}
 
 }
