871989a12270fd75414b22ec9d44a4e992f5c088
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-08-18 11:42:28 +0200

    modifier l'écoute du clic pour l'ouverture et la fermeture de la modal de contact de HTML à JavaScript

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
-
+document.getElementById('openModal') .addEventListener('click',displayModal)
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
+document.getElementById('closeModal') .addEventListener('click',closeModal)
+
+
